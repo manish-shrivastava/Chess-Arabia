b3b9d2c7b1e852aa74b2d06d1cae44a3f730e576
Omar Qunsul <okonsul@gmail.com>
2011-08-30 00:17:16 +0300

    Top players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
 
   def top
-    @players = User.order('rank DESC').limit(15)
+    @players = User.order('rating DESC').limit(15)
   end
 
   def edit
@@ -13,6 +13,10 @@ class PlayersController < ApplicationController
     sign_in(current_user, :bypass => true)
     redirect_to edit_player_path(current_user)
     flash[:notice] = "Profile updated successfully"
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def my_rating
